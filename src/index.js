ead054cde7ef940a4f6a2469883218874d943b1a
Michael N. Rubinstein <michael@polyblog.com>
2019-04-08 17:31:37 -0700

    Completed section 2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getTime() {
+    return (new Date()).toLocaleTimeString()
+}
+
 // Create a React component
 const App = function() {
-    return (<div>Hi there!</div>);
+    const textAssets = {
+        button: 'Click me!',
+        label: 'Enter name:'
+    };
+
+    return (
+        <div>
+            <h3>Current time: {getTime()}</h3>
+            <label className="label" htmlFor="name">
+                {textAssets.label}
+            </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                {textAssets.button}
+            </button>
+        </div>
+    );
 };
 
 // Take the React component and show it on the screen
